425cc7acb16243352853e971c9f86f0bbd58aa04
alex-obada <alex2539gg@gmail.com>
2023-03-27 18:02:55 +0300

    actual random generation

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -30,6 +30,7 @@ MainWindow::MainWindow(QWidget *parent)
     for(size_t i = 0; i < NR; ++i)
         numbers[i] = i + 1;
 
+    srand(time(NULL));
     std::random_shuffle(numbers.begin(), numbers.end());
     for(size_t i = 0; i < NR; ++i)
         elements[i].value = numbers[i];
@@ -37,17 +38,8 @@ MainWindow::MainWindow(QWidget *parent)
     for(auto& element : elements)
     {
         element.ptr = new QWidget();
-#if 0
-        static bool culoare = false;
-        if(culoare)
-            element->setStyleSheet("background-color: red;"); // margin: 0; padding: 0; border: 0;");
-        else
-            element->setStyleSheet("background-color: blue;"); // margin: 0; padding: 0; border: 0;");
-        culoare = !culoare;
-#endif
         element.ptr->setStyleSheet("background-color: black;");
         element.ptr->setMaximumHeight(element.value * element.ptr->height() / NR);
-
         layout->addWidget(element.ptr);
     }
 
